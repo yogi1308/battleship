f1b3e62912bc7df304641e23fb52a86a330d04ba
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-05-21 10:58:03 -0700

    UI - Added a loading screen
    - added a loading screen from https://css-loaders.com/spinner/
    - achieved it using inline css and js. reasoning below
        - added inline css and js to html file so that the loading screen correctly displays before the whole page loads
        - Inline CSS is used to avoidd Flash of Unstyled Content (FOUC) as external CSS is loaded after the HTML is parsed.
        - With inline styles, the loader’s size, position, and background are applied as soon as the browser reads the HTML, ensuring no visual jump or delay.
        - Inline JS to ensure Script Executes Immediately: If JS is external or deferred, it runs after the page is fully parsed or even after assets load, which could delay hiding the loader.
        - No Dependency on Module Loaders / Webpack Timing: When using bundlers like Webpack, your actual logic may be wrapped in asynchronous code (especially with type="module"), which adds slight delay.

---
+++ src/script/ui/loadingScreen.js
@@ -0,0 +1,4 @@
+export function removeLoadingScreen() {
+    document.querySelector('.loading-screen').style.display = 'none';
+    document.querySelector('.main-screen').style.display = 'block';
+}
