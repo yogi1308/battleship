047f337550df94a13f3ca4e780480e0b54648509
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-05-25 15:37:47 -0700

    UI - improved the ship lacement ui to be compatible and not break in various device sizes and some more features to it
    - added various ships icons from which the user can select where to place the ship
    - made it responsive so that the user knows when the ship is selected
    - did the same with axis and made x-axis, the default one.
    - added media queries to make it work with various viewports

--- src/script/ui/placeShips.js
+++ src/script/ui/placeShips.js
@@ -3,15 +3,47 @@ export function placeShips(numPlayers) {
         document.querySelector('.player-name').style.display = 'none'
         const placeShips = document.querySelector('.place-ships')
         placeShips.style.display = 'flex';
-        placeShips.querySelector('.done').style.width = (document.querySelector('div.place-ships > div.grid').offsetWidth) + 'px';
+        document.querySelector('.axis > .x-axis').style.color = 'rgb(4, 29, 33)'
+        document.querySelector('.axis > .x-axis').style.background = 'rgb(80, 207, 208)';
         document.querySelector('.place-ship-player-name').textContent = document.querySelector('.player-name-input').value
+        document.querySelectorAll('.ship-pallete > div').forEach(div => {
+            div.addEventListener('click', shipClicked);
+        });
+        document.querySelectorAll('.axis > button').forEach(button => {
+            button.addEventListener('click', axisClicked);
+        });
     }
     else {
         const placeShips = document.querySelector('.place-ships')
         document.querySelector('.player-name').style.display = 'none'
         placeShips.style.display = 'flex';
-        placeShips.querySelector('.done').style.width = (document.querySelector('div.place-ships > div.grid').offsetWidth) + 'px';
+        document.querySelector('.axis > .x-axis').style.color = 'rgb(4, 29, 33)'
+        document.querySelector('.axis > .x-axis').style.background = 'rgb(80, 207, 208)';
         document.querySelector('.place-ship-player-name').textContent = document.querySelector('.player-name-one-input').value
         placeShips.querySelector('.done').addEventListener('click', () => document.querySelector('.place-ship-player-name').textContent = document.querySelector('.player-name-two-input').value)
+                document.querySelectorAll('.ship-pallete > div').forEach(div => {
+            div.addEventListener('click', shipClicked);
+        });
+        document.querySelectorAll('.axis > button').forEach(button => {
+            button.addEventListener('click', axisClicked);
+        });
     }
+}
+
+function shipClicked(event) {
+    document.querySelectorAll('.ship-pallete img').forEach(img => {
+        img.style.filter = ''; // reset others
+    });
+    const img = event.currentTarget.querySelector('img');
+    img.style.filter = 'brightness(0) invert(1)';
+}
+
+function axisClicked(event) {
+    document.querySelectorAll('.axis > button').forEach(button => {
+        button.style.background = ''; // reset others
+        button.style.color = ''
+    });
+    const button = event.currentTarget;
+    button.style.background = 'rgb(80, 207, 208)';
+    button.style.color = 'rgb(4, 29, 33)'
 }
